79c81e48505c687504c2d5c5f7b9d0f4eb3b7513
javier <javier.cestau1209@gmail.com>
2018-06-23 02:08:10 -0400

    testing forgery

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -2,7 +2,7 @@
 
 if Rails.env.production?
   ActionMailer::Base.asset_host = ::CatarseSettings.get_without_cache(:base_url)
-  Rails.application.routes.default_url_options = { host: ::CatarseSettings.get_without_cache(:host),  protocol: 'https' }GIT
+  Rails.application.routes.default_url_options = { host: ::CatarseSettings.get_without_cache(:host),  protocol: 'https' }
 else
   Rails.application.routes.default_url_options = { host: 'localhost:3000' }
 end
